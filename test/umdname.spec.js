7bbfd8f8c70dd99c5e0a06ec95597ede1cc29607
chris <chris@bumblehead.com>
2016-02-11 01:55:41 -0800

    updated umdname to process rx-all.js

--- test/umdname.spec.js
+++ test/umdname.spec.js
@@ -1,5 +1,5 @@
 // Filename: umdname.spec.js  
-// Timestamp: 2015.12.19-15:42:30 (last modified)
+// Timestamp: 2016.02.11-01:54:15 (last modified)
 // Author(s): bumblehead <chris@bumblehead.com>
 
 var umdname = require('../');
@@ -24,6 +24,40 @@ var umd_mustache = [
   '}));'
 ].join('\n');
 
+var umd_rxall = [
+  ';(function (undefined) {',
+  '  var objectTypes = {',
+  '    \'function\': true,',
+  '    \'object\': true',
+  '  };',
+  '',
+  '  function checkGlobal(value) {',
+  '    return (value && value.Object === Object) ? value : null;',
+  '  }',
+  '  ',
+  '  if (typeof define == \'function\' && typeof define.amd == \'object\' && define.amd) {',
+  '    root.Rx = Rx;',
+  '',
+  '    define(function() {',
+  '      return Rx;',
+  '    });',
+  '  } else if (freeExports && freeModule) {',
+  '    // in Node.js or RingoJS',
+  '    if (moduleExports) {',
+  '      (freeModule.exports = Rx).Rx = Rx;',
+  '    } else {',
+  '      freeExports.Rx = Rx;',
+  '    }',
+  '  } else {',
+  '    // in a browser or Rhino',
+  '    root.Rx = Rx;',
+  '  }',
+  '',
+  '  // All code before this point will be filtered from stack traces.',
+  '  var rEndingLine = captureLine();',
+  '',
+  '}.call(this));'
+].join('\n');
 
 describe("umdname", function () {
   it("should adapt the mustche.js UMD boilerplate to use namespace 'speedracer'", function () {
@@ -31,5 +65,16 @@ describe("umdname", function () {
 
      expect(/Mustache/gm.test(content)).toBe(false);
      expect(/speedracer/gm.test(content)).toBe(true);     
+  });
+});
+
+describe("rxjsname", function () {
+  it("should adapt the rx-all.js UMD boilerplate to use namespace 'speedracer'", function () {
+    var content = umdname(umd_rxall, 'speedracer');
+
+    console.log('content is ', content);
+    
+     expect(/root.Rx/gm.test(content)).toBe(false);
+     expect(/root.speedracer/gm.test(content)).toBe(true);     
   });
 });
--- umdname.js
+++ umdname.js
@@ -1,5 +1,5 @@
 // Filename: umdname.js  
-// Timestamp: 2016.02.10-17:18:58 (last modified)
+// Timestamp: 2016.02.11-01:55:30 (last modified)
 // Author(s): bumblehead <chris@bumblehead.com>  
 //
 // official umd templates,
@@ -34,7 +34,8 @@ var umdname = module.exports = (function (o) {
   };
 
   o.template_with_namespace = function (umd_expression_ast, namespace) {
-    var global = umd_expression_ast.params[0].name;
+    var global = umd_expression_ast.params &&
+                 umd_expression_ast.params[0].name || 'root';
     
     return estraverse.replace(umd_expression_ast, {
       leave: function leave(node, parent) {
@@ -51,12 +52,13 @@ var umdname = module.exports = (function (o) {
   
   o.content_withnamespace = function (content, namespace) {
     var ast = esprima.parse(content);
-
-    for (var o = 0; o < ast.body.length; o++) {
-      if (ast.body[o].type === 'CallExpression') {
-        ast.body[o].expression.callee =
-          o.template_with_namespace(ast.body[o].expression.callee, namespace);
-        
+    
+    // this expression and function expression as args
+    for (var x = 0; x < ast.body.length; x++) {
+      if (ast.body[x].type === 'ExpressionStatement' &&
+          ast.body[x].expression.type === 'CallExpression') {
+        ast.body[x].expression.callee =
+          o.template_with_namespace(ast.body[x].expression.callee, namespace);
         break;
       }
     }
